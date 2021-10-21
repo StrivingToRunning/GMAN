384d5cfb50967cb4d8c3c99687f46a44b6b0ed54
xubowei <xubowei@kkworld.com>
2021-10-21 23:48:39 +0800

    stash

--- PeMS/train.py
+++ PeMS/train.py
@@ -34,7 +34,7 @@ parser.add_argument('--learning_rate', type=float, default = 0.001,
                     help = 'initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default = 5,
                     help = 'decay epoch')
-parser.add_argument('--traffic_file', default = '../input/pemsh5/PeMS.h5',
+parser.add_argument('--traffic_file', default = '/kaggle/input/pemsh5/PeMS.h5',
                     help = 'traffic file')
 parser.add_argument('--SE_file', default = 'data/SE(PeMS).txt',
                     help = 'spatial emebdding file')
