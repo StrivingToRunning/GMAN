b946375b98c7f1af738e984c5e7f26894adec99e
xubowei <xubowei@kkworld.com>
2021-10-22 00:00:38 +0800

    stash

--- PeMS/utils.py
+++ PeMS/utils.py
@@ -71,7 +71,7 @@ def loadData(args):
     Time = df.index
     dayofweek =  np.reshape(Time.weekday, newshape = (-1, 1))
     timeofday = (Time.hour * 3600 + Time.minute * 60 + Time.second) \
-                // Time.freq.delta.total_seconds()
+                // (5*60) # Time.freq.delta.total_seconds()
     timeofday = np.reshape(timeofday, newshape = (-1, 1))    
     Time = np.concatenate((dayofweek, timeofday), axis = -1)
     # train/val/test
